2c35ba841e0e74807c66efc91e75610712f1aacd
susguzman <susguzman@gmail.com>
2015-04-03 20:30:50 -0600

    - creados paquetes principales - creada clase tablero y main

---
+++ FubtolIA/src/gui/PanelCampo.java
@@ -0,0 +1,79 @@
+package gui;
+
+import java.awt.BasicStroke;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Jesus
+ */
+public class PanelCampo extends JPanel{
+    
+    private final int ANCHO = 897, ALTO = 600;  
+    private double MARGEN_BASE_TOP = 13, MARGEN_BASE_RIGTH = 47;
+    private double MARGEN_TOP, MARGEN_RIGTH;
+    private final ImageIcon CAMPO = new ImageIcon(getClass().getResource("/img/campo.jpg"));
+    
+    public PanelCampo(){
+        super();
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        
+        //Calcular Margene lados
+        if(this.getWidth() != ANCHO){
+            double porcentaje = (this.getWidth() * 100) /ANCHO;
+            if(porcentaje > 100){
+                MARGEN_RIGTH = MARGEN_BASE_RIGTH + ((MARGEN_BASE_RIGTH * (porcentaje - 100))/100);
+            }else if(porcentaje < 100){
+                MARGEN_RIGTH = MARGEN_BASE_RIGTH - ((MARGEN_BASE_RIGTH * (100 - porcentaje))/100);
+            }else{
+                MARGEN_RIGTH = MARGEN_BASE_RIGTH;
+            }
+            //System.out.println("Margen rig:" + MARGEN_RIGTH);
+        }else{
+            MARGEN_RIGTH = MARGEN_BASE_RIGTH;
+        }
+        
+        //Calcular Margene superior
+        if(this.getHeight() != ALTO){
+            double porcentaje = (this.getHeight() * 100) /ALTO;
+            System.out.println("por " + porcentaje);
+            if(porcentaje > 100){
+                MARGEN_TOP = MARGEN_BASE_TOP + ((MARGEN_BASE_TOP * (porcentaje - 100))/100);
+            }else if(porcentaje < 100){
+                MARGEN_TOP = MARGEN_BASE_TOP - ((MARGEN_BASE_TOP * (100 - porcentaje))/100);
+            }else{
+                MARGEN_TOP = MARGEN_BASE_TOP;
+            }
+        }else{
+            MARGEN_TOP = MARGEN_BASE_TOP;
+        }
+        
+        //Dibujar campo
+        g.drawImage(CAMPO.getImage(), 0, 0, super.getWidth(), super.getHeight(), this);
+        
+        double ancho = (this.getWidth() - MARGEN_RIGTH * 2) / 17;
+        double alto = (this.getHeight() - MARGEN_TOP * 2) / 11;
+        
+        //Dibujar lineas verticales 17       
+        g2d.setStroke(new BasicStroke(2f));
+        
+        for (int i = 0; i < 18; i++) {
+            g2d.drawLine((int)(i * ancho) + (int)MARGEN_RIGTH, (int)MARGEN_TOP, (int)(i * ancho) + (int)MARGEN_RIGTH, this.getHeight() - (int)MARGEN_TOP);            
+        }
+        
+        //Dibujar lineas horizonatal 11
+        for (int i = 0; i < 13; i++) {
+            g2d.drawLine((int)MARGEN_RIGTH, (i * (int)alto) + (int)MARGEN_TOP, this.getWidth() - (int)MARGEN_RIGTH, (i * (int)alto) + (int)MARGEN_TOP);            
+        }
+        
+        super.paintComponent(g);
+    }
+    
+}
--- FubtolIA/src/gui/Tablero.java
+++ FubtolIA/src/gui/Tablero.java
@@ -1,5 +1,7 @@
 package gui;
 
+import java.awt.Color;
+
 /**
  *
  * @author Jesus
@@ -11,6 +13,7 @@ public class Tablero extends javax.swing.JFrame {
      */
     public Tablero() {
         initComponents();
+        myInit();
     }
 
     /**
@@ -22,30 +25,129 @@ public class Tablero extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jTabbedPane1 = new javax.swing.JTabbedPane();
+        jPanel3 = new javax.swing.JPanel();
+        jPanel4 = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Simulador de Partido");
+        setTitle("Simulador de Pardio v0.23");
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        jPanel2.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.LOWERED));
+        jPanel2.addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentResized(java.awt.event.ComponentEvent evt) {
+                jPanel2ComponentResized(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 895, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 228, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 570, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.addTab("Control", jPanel3);
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 228, Short.MAX_VALUE)
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 570, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.addTab("Conexion", jPanel4);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 237, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jTabbedPane1))
+                .addContainerGap())
+        );
+
+        jTabbedPane1.getAccessibleContext().setAccessibleName("");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        
-    }
+    private void jPanel2ComponentResized(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_jPanel2ComponentResized
+        p_campo.setSize(jPanel2.getWidth() - 4, jPanel2.getHeight() - 4);
+    }//GEN-LAST:event_jPanel2ComponentResized
 
+    private void myInit(){
+        //Dibujar campo
+        p_campo = new PanelCampo();
+        p_campo.setBounds(2, 2, jPanel2.getWidth() - 4, jPanel2.getHeight() - 4);
+        p_campo.setOpaque(false);
+        jPanel2.add(p_campo);
+        
+        jPanel2.updateUI();
+    }
+    
+    //Mis Variables
+    private PanelCampo p_campo;
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JTabbedPane jTabbedPane1;
     // End of variables declaration//GEN-END:variables
 }
